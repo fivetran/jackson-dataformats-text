4456f8e5e8cfbc744cf8275c8bfc7364ba00e31b
Peter Jankovsky <peterj@fivetran.com>
2019-06-25 18:23:25 -0700

    Add ability to limit token length

--- csv/src/main/java/com/fasterxml/jackson/dataformat/csv/impl/CsvDecoder.java
+++ csv/src/main/java/com/fasterxml/jackson/dataformat/csv/impl/CsvDecoder.java
@@ -85,6 +85,8 @@ public class CsvDecoder
 
     protected int _escapeChar;
 
+    protected int _maxTokenLength = -1;
+
     /*
     /**********************************************************************
     /* Input handling, state
@@ -179,6 +181,8 @@ public class CsvDecoder
      * in the end it'll be converted to 1-based)
      */
     protected int _tokenInputCol = 0;
+
+    protected int _tokenLength = 0;
 
     /*
     /**********************************************************************
@@ -310,6 +314,7 @@ public class CsvDecoder
         _separatorChar = schema.getColumnSeparator();
         _quoteChar = schema.getQuoteChar();
         _escapeChar = schema.getEscapeChar();
+        _maxTokenLength = schema.getMaxTokenLength();
         if (!_allowComments) {
             _allowComments = schema.allowsComments();
         }
@@ -664,6 +669,7 @@ public class CsvDecoder
     public String nextString() throws IOException
     {
         _numTypesValid = NR_UNKNOWN;
+        _tokenLength = 0;
         
         if (_pendingLF > 0) { // either pendingLF, or closed
             if (_inputReader != null) { // if closed, we just need to return null
@@ -704,6 +710,7 @@ public class CsvDecoder
         }
         char[] outBuf = _textBuffer.emptyAndGetCurrentSegment();
         outBuf[0] = (char) i;
+        _tokenLength++;
         int outPtr = 1;
 
         if (i == _escapeChar) {
@@ -747,7 +754,10 @@ public class CsvDecoder
                     break;
                 }
             }
-            outBuf[outPtr++] = c;
+            if (_maxTokenLength == -1 || _tokenLength < _maxTokenLength) {
+                outBuf[outPtr++] = c;
+                _tokenLength++;
+            }
         }
         // ok, either input or output across buffer boundary, offline
         _inputPtr = ptr;
@@ -831,7 +841,10 @@ public class CsvDecoder
                         continue main_loop;
                     }
                 }
-                outBuf[outPtr++] = (char) c;
+                if (_maxTokenLength == -1 || _tokenLength < _maxTokenLength) {
+                    outBuf[outPtr++] = (char) c;
+                    _tokenLength++;
+                }
             }
             _inputPtr = ptr;
         }
@@ -899,12 +912,18 @@ public class CsvDecoder
                     } else if (c == _escapeChar) {
                         _inputPtr = ptr;
                         c = _unescape();
-                        outBuf[outPtr++] = c;
+                        if (_maxTokenLength == -1 || _tokenLength < _maxTokenLength) {
+                            outBuf[outPtr++] = c;
+                            _tokenLength++;
+                        }
                         // May have passed input boundary, need to re-set
                         continue main_loop;
                     }
                 }
-                outBuf[outPtr++] = c;
+                if (_maxTokenLength == -1 || _tokenLength < _maxTokenLength) {
+                    outBuf[outPtr++] = c;
+                    _tokenLength++;
+                }
                 if (ptr >= max) {
                     _inputPtr = ptr;
                     continue main_loop;
@@ -915,7 +934,10 @@ public class CsvDecoder
             if (_inputPtr < _inputEnd || loadMore()) { 
                 if (_inputBuffer[_inputPtr] == _quoteChar) { // doubled up, append
                     // note: should have enough room, is safe
-                    outBuf[outPtr++] = (char) _quoteChar;
+                    if (_maxTokenLength == -1 || _tokenLength < _maxTokenLength) {
+                        outBuf[outPtr++] = (char) _quoteChar;
+                        _tokenLength++;
+                    }
                     ++_inputPtr;
                     continue main_loop;
                 }
